21ac2c17d7994c6c8772b61100f6b34c211fc08a
Axel Duriez <axel.duriezjv@gmail.com>
2020-12-16 23:58:50 -0500

    le css de la page est terminé. L'intégration de the moovie db à été commencée.

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,6 @@
 
 document.addEventListener("DOMContentLoaded", function(event){
 
-    //script pour la fleche qui remonte
     document.querySelector('#remonte').addEventListener("click", fscroll);
 
     function fscroll(e){
@@ -13,12 +12,10 @@ document.addEventListener("DOMContentLoaded", function(event){
             behavior:'smooth'
         })
     }
-    //Script pour la transition smooth vers la section contact dans le footer
+
     var destination;
 
     var boutondeux =document.querySelector('#cont');
-
-    boutondeux.addEventListener("click", fscrolll);
 
     function fscrolll(pEvt){
         var cible = pEvt.currentTarget.getAttribute("id");
@@ -41,13 +38,22 @@ document.addEventListener("DOMContentLoaded", function(){
 
     let connexion = new MovieDB();
 
-    connexion.requeteDernierFilm();
+    if(document.location.pathname.search("fiche-film.html") > 0){
+        let params = ( new URL(document.location) ).searchParams;
+
+        console.log(params);
+        //connexion.requeteInfosFilm();
+    }else{
+        connexion.requeteDernierFilm();
+    }
 
 })
 
 class MovieDB{
 
     constructor() {
+
+        console.log("Constructeur")
 
         this.APIkey = "2ed19bb8abf51968b90eb9a455fdc344";
 
@@ -57,7 +63,7 @@ class MovieDB{
 
         this.imgPath = "https://image.tmdb.org/t/p/";
 
-        this.totalFilm = 9;
+        this.totalFilm = 8;
 
     }
 
@@ -65,21 +71,22 @@ class MovieDB{
 
         let requete = new XMLHttpRequest();
 
-        requete.addEventListener("loaded", this.retourRequeteDernierFilm.bind(this));
+        requete.addEventListener("loadend", this.retourRequeteDernierFilm.bind(this) );
 
-        requete.open("GET", this.baseURL + "movie/now_playing?page=1&language=" + this.lang + "&&api_key" + this.APIkey);
+        requete.open("GET", "https://api.themoviedb.org/3/movie/now_playing?api_key=2ed19bb8abf51968b90eb9a455fdc344&language=fr-Ca&page=1");
+
+        //requete.open("GET", this.baseURL + "/movie/now_playing?api_key=" + this.APIkey + "&language=" +this.lang + "&page=1");
 
         requete.send();
 
     }
 
-    //https://web.microsoftstream.com/video/e305022a-c0ad-4e07-b433-2814bdc309c4?list=user&userId=a9537877-fe51-44f4-9a14-520d6a15864c 9min20
-
     retourRequeteDernierFilm(e){
 
-        console.log("Retour dernier Film")
+        console.log("retour dernier film");
 
         let target = e.currentTarget;
+
         let data;
 
         //console.log(target.responseText);
@@ -94,10 +101,79 @@ class MovieDB{
 
     afficheDernierFilm(data){
 
-        for (let i = 0; i < data.lenght; i++) {
-            console.log(data[i].title);
+        for (let i = 0; i < this.totalFilm; i++) {
+
+            let unArticle = document.querySelector(".template>article.film").cloneNode(true);
+
+            unArticle.querySelector("h2").innerHTML = data[i].title;
+
+            unArticle.querySelector("p.description").innerHTML = data[i].overview || "Pas de description disponnible";
+
+            let src = this.imgPath + "w185" + data[i].poster_path;
+
+            let uneImage = unArticle.querySelector("img");
+            uneImage.setAttribute("src", src);
+            uneImage.setAttribute("alt", data[i].title);
+
+            document.querySelector(".liste-films").appendChild(unArticle);
+
+
         }
 
+    }
+
+    requeteInfosFilm(movieId){
+
+        let requete = new XMLHttpRequest();
+
+        requete.addEventListener("loadend", this.retourRequeteInfosFilm.bind(this) );
+
+        requete.open("GET", "https://api.themoviedb.org/3/movie/now_playing?api_key=2ed19bb8abf51968b90eb9a455fdc344&language=fr-Ca&page=1");
+
+        //requete.open("GET", this.baseURL + "/movie/now_playing?api_key=" + this.APIkey + "&language=" +this.lang + "&page=1");
+
+        requete.send();
+
+    }
+
+    retourRequeteInfosFilm(e){
+
+        console.log("retour dernier film");
+
+        let target = e.currentTarget;
+
+        let data;
+
+        //console.log(target.responseText);
+
+        data = JSON.parse(target.responseText).results;
+
+        console.log(data);
+
+        this.afficheInfosFilm(data);
+
+    }
+
+    afficheInfosFilm(data) {
+
+        // for (let i = 0; i < this.totalFilm; i++) {
+        //
+        //     let unArticle = document.querySelector(".template>article.film").cloneNode(true);
+        //
+        //     unArticle.querySelector("h2").innerHTML = data[i].title;
+        //
+        //     unArticle.querySelector("p.description").innerHTML = data[i].overview || "Pas de description disponnible";
+        //
+        //     let src = this.imgPath + "w185" + data[i].poster_path;
+        //
+        //     let uneImage = unArticle.querySelector("img");
+        //     uneImage.setAttribute("src", src);
+        //     uneImage.setAttribute("alt", data[i].title);
+        //
+        //     document.querySelector(".liste-films").appendChild(unArticle);
+        //
+        //
+        // }
     }
 
 }
